66bf6bcc5a0ea54fe752604f72a8064905d734a9
Seth Whalen <83984671+swhalen-invn@users.noreply.github.com>
2021-10-05 17:36:11 -0600

    Revert "Dev branch dfgdgerg"

--- HelloWorld/src/HelloWorld.c
+++ HelloWorld/src/HelloWorld.c
@@ -6,15 +6,11 @@ int main() {
     printf("Hello, Jenkins webhooks!\n");
     printf("Let us do some addition...\n");
 
-    int x = 10;
-    int y = 2
-    printf("Computing %d + %d...", x, y);
-    int total = add(x, y);
+    printf("Computing 11 + 5...");
+    int total = add(11,5);
     printf("The answer is: %d!!\n", total);
 
-    int a = 11;
-    int b = 6;
-    printf("Computing %d x %d...", a, b);
+    printf("Computing 8 x 6...");
     total = multiply(8,6);
     printf("The answer is: %d!!\n", total);
     return 0;
